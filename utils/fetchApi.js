93a1ad877167c52c6bae6b5f4b7b84fc1a231149
Alfredkt <alfredtawia2244@gmail.com>
2024-07-19 13:42:08 +0100

    first commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -4,8 +4,8 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 
 export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
-    headers: {
-      'x-rapidapi-key': '4f9eed92b0mshfa5338b290a8878p1e8920jsn4320dee3ed4d',
+    headers:  {
+      'x-rapidapi-key': '643715cc10mshabfc14f4edf331bp12b3e5jsn991aece1118e',
       'x-rapidapi-host': 'bayut.p.rapidapi.com'
     },
   });
